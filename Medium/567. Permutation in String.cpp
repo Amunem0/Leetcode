2543bf0b975b1455036189727d16085a82be65b5
Aryan Kashikar <115734963+Amunem0@users.noreply.github.com>
2024-10-05 13:58:06 +0530

    Update 567. Permutation in String.cpp

--- Medium/567. Permutation in String.cpp
+++ Medium/567. Permutation in String.cpp
@@ -32,3 +32,26 @@ public:
         
     }
 };
+
+
+//APPROACH 2
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int n=s1.length();
+        int m=s2.length();
+
+        sort(s1.begin(),s1.end());
+
+        for(int i=0;i<=m-n;i++){
+            string substring=s2.substr(i,n);
+
+            sort(substring.begin(),substring.end());
+
+            if(substring==s1){
+                return true;
+            }
+        }
+        return false;
+    }
+};
