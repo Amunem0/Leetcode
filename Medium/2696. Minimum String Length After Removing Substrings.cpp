eb56f663e844bb961926400283be284a061ef9db
Aryan Kashikar <115734963+Amunem0@users.noreply.github.com>
2024-10-07 14:57:12 +0530

    Update 2696. Minimum String Length After Removing Substrings.cpp

--- Medium/2696. Minimum String Length After Removing Substrings.cpp
+++ Medium/2696. Minimum String Length After Removing Substrings.cpp
@@ -19,3 +19,49 @@ public:
         return st.size();
     }
 };
+
+
+//APPROACH 2
+// class Solution {
+// public:
+//     int minLength(string s) {
+//         stack<char> st;
+//         for (int i = 0; i < s.length(); i++) {
+//             if (!st.empty()) {
+//                 if (st.top() == 'A' && s[i] == 'B' ||
+//                     st.top() == 'C' && s[i] == 'D') {
+//                     st.pop();
+//                 } else {
+//                     st.push(s[i]);
+//                 }
+//             } else {
+//                 st.push(s[i]);
+//             }
+//         }
+//         return st.size();
+//     }
+// };
+
+class Solution {
+public:
+    int minLength(string s) {
+        int i = 0;
+        int j = 1;
+        int n = s.length();
+
+        while (j < n) {
+            if (i < 0) {
+                i++;
+                s[i] = s[j];
+            }
+            else if ((s[i] == 'A' && s[j] == 'B') || (s[i] == 'C' && s[j] == 'D')) {
+                i--;
+            } else {
+                i++;
+                s[i] = s[j];
+            }
+            j++;
+        }
+        return i + 1;
+    }
+};
