0aff57b5ad7706353fffd4546a43fe668ad91653
Aryan Kashikar <115734963+Amunem0@users.noreply.github.com>
2024-12-19 02:03:57 +0530

    Create 1475. Final Prices With a Special Discount in a Shop.cpp

---
+++ Easy/1475. Final Prices With a Special Discount in a Shop.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        int n = prices.size();
+        stack<int> st;
+        vector<int> ans(prices.begin(),prices.end());
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && prices[i] <= prices[st.top()]) {
+                ans[st.top()] -=  prices[i];
+                st.pop();
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+};
