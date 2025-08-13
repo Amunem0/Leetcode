097f8cf3643afb5784db19d4a5fe6542cf85cd04
Aryan Kashikar <115734963+Amunem0@users.noreply.github.com>
2025-08-13 10:10:51 +0530

    Create 326. Power of Three.cpp

---
+++ Easy/326. Power of Three.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n<=0){
+            return false;
+        }
+        while(n>0){
+            if(n==1){
+                return true;
+            }
+            if(n%3!=0) {
+                return false;
+            }
+            else{
+                n=n/3;
+            }
+        }
+        return true;
+    }
+};
